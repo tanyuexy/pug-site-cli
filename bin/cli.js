8eee18c93341813b23ad12a711fdc2b408a83468
xy <xiaoyi@tanyue.cc>
2025-02-17 18:27:22 +0800

    版本号从package.json中获取

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const create = require('../lib/create');
+const program = require("commander");
+const create = require("../lib/create");
+const { version } = require("../package.json");
 
 program
-  .version('1.0.0')
-  .command('create <project-name>')
-  .description('创建一个新项目')
+  .version(version)
+  .command("create <project-name>")
+  .description("创建一个新项目")
   .action((projectName) => {
     create(projectName);
   });
 
-program.parse(process.argv); 
+program.parse(process.argv);
